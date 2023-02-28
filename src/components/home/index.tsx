1941e848f4d601ebce6c795125fc0000ab97fd4f
ryogazz <josuex.x.x@hotmail.com>
2023-02-28 15:58:29 -0300

    feat: pagina inicial terminada

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,31 +1,24 @@
 import * as S from "./style";
-import background from "../../assets/home/background-home-desktop.jpg";
+
 const Home = () => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <S.Container>
-        <S.Heading5>SO, YOU WANT TO TRAVEL TO</S.Heading5>
-        <S.Heading1>SPACE</S.Heading1>
-        <S.BodyText>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </S.BodyText>
-      </S.Container>
-      <S.Circle>
-        <S.TextCircle>Explorer</S.TextCircle>
-      </S.Circle>
-    </div>
+    <S.Background>
+      <S.TextAndCircle>
+        <S.Container>
+          <S.Heading5>SO, YOU WANT TO TRAVEL TO</S.Heading5>
+          <S.Heading1>SPACE</S.Heading1>
+          <S.BodyText>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </S.BodyText>
+        </S.Container>
+        <S.Circle>
+          <S.TextCircle>Explorer</S.TextCircle>
+        </S.Circle>
+      </S.TextAndCircle>
+    </S.Background>
   );
 };
 
--- src/components/home/style.tsx
+++ src/components/home/style.tsx
@@ -1,4 +1,15 @@
 import styled from "styled-components";
+import background from "../../assets/home/background-home-desktop.jpg";
+
+export const Background = styled.div`
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
 
 export const Heading1 = styled.h1`
   font-family: "Bellefair";
@@ -24,7 +35,6 @@ export const Heading5 = styled.div`
 
 export const BodyText = styled.p`
   color: #ffffff;
-  position: absolute;
   height: 128px;
   left: 0.11%;
   right: 1.22%;
@@ -37,41 +47,40 @@ export const BodyText = styled.p`
 `;
 
 export const Container = styled.div`
-  position: absolute;
   width: 450px;
   height: 382px;
-  left: 165px;
+  /* left: 165px; */
   top: 387px;
 `;
 
 export const Circle = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #ffffff;
   border-radius: 50%;
-  position: absolute;
   width: 274px;
   height: 274px;
-  left: 1301px;
-  top: 495px;
   :hover {
     box-shadow: 0 0 10px yellow;
   }
 `;
 
 export const TextCircle = styled.div`
-  position: absolute;
   height: 37px;
-  width: 148px;
-  top: 119px;
-  left: 27.63%;
-  right: 23.36%;
-
   font-family: "Bellefair";
   font-style: normal;
   font-weight: 400;
   font-size: 32px;
   line-height: 37px;
   letter-spacing: 2px;
-
   color: #0b0d17;
+  text-transform: uppercase;
+`;
+
+export const TextAndCircle = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100vw;
+  margin-top: 500px;
 `;
